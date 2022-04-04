0c58018f9add06305081d666c7c03ca7601e4394
Elegbede Olasubomi <shubbynlaa@gmail.com>
2022-04-04 15:22:32 +0100

    Added the front image and intro .

--- src/Components/home.jsx
+++ src/Components/home.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Form from "./form";
+import "./home.css";
 
 function Home() {
   return (
     <div>
       <div className="container">
         <div className="row">
-          <div className="col">1 of 2</div>
+          <div className="col ">
+            <div>
+              <h1 className="edufunda">Edufunda</h1>
+              <img src="learning.svg" className="introImage" />
+              <div className="intro">
+                <h1>Assignment Solution</h1>
+                <p>
+                  It is a long established fact that a reader will be <br />
+                  distracted by the readable content
+                </p>
+              </div>
+            </div>
+          </div>
           <div className="col">
             <Form />
           </div>
